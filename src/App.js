58791f1211ea87dae8bb3966206f5dc0ac1d5b57
manichand <148627679+manichand3@users.noreply.github.com>
2026-03-25 06:37:11 +0000

    completed counter basic app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  function handleDecrement() {
+    setCount((prev) => Math.max(prev - 1, 0));
+  }
+  function handleIncrement() {
+    setCount((prev) => prev + 1);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>react counter</h1>
+      <p>counter {count}</p>
+      <button onClick={handleIncrement}>increment</button>
+      {count > 0 && <button onClick={handleDecrement}>decrement </button>}
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        reset{" "}
+      </button>
     </div>
   );
 }
